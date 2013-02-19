0cd9c32574f357463762b27084cc3e2f4f8b66b9
Liz Howard <ann.howard@me.com>
2013-02-19 12:12:44 -0800

    Adding tarball christian gave me so we have everything in one place

--- Exercise05/arithmetic.py
+++ Exercise05/arithmetic.py
@@ -1,3 +1,32 @@
-def add(first_number, second_number):
-    print "DEBUG MESSAGE: The add function is not defined"
-    return 0
+def add(num1, num2):
+	return num1 + num2
+
+def subtract(num1, num2):
+	return num1 - num2
+
+def multiply(num1, num2):
+	return num1 * num2
+
+def divide(num1, num2):
+	return float(num1) / num2
+
+def square(num1):
+	return num1 * num1
+
+def cube(num1):
+	return num1 * num1 * num1
+
+def power(num1, num2):
+	result = 1
+	for i in range(num2):
+		result = result * num1
+		#result = 1*num1 = num1
+		#result = num1 * num1 = num1^2
+		#result = 
+	return result
+
+def mod(num1, num2):
+	return num1 % num2
+  
+
+
--- Exercise08/task7.py
+++
@@ -1,9 +0,0 @@
-#!/bin/env python
-
-"""
-Given the list l composed of tuples, sort the list by the second item in the tuple
-    l = [(1,3), (3,2), (5,1)]
-    becomes
-    l = [(5,1), (3,2), (1,3)]
-"""
-l = [(1,2), (3,1), (17, 35), (81,20)]
