d1e1981aeb0cf783c19d4345b5ae976f2577ac75
chriszf <chriszf@gmail.com>
2013-10-03 09:58:08 -0700

    Adding recursive exercise

---
+++ Exercise09/recursion.py
@@ -0,0 +1,38 @@
+# Multiply all the elements in a list
+def factorial(l):
+    return 1
+
+# Count the number of elements in the list l
+def count_list(l):
+    return
+
+# Sum all of the elements in a list
+def sum_list(l):
+    return 0
+
+
+# Reverse a list without slicing or loops
+def reverse(l):
+    return []
+
+# Fibonacci returns the nth fibonacci number. The nth fibonacci number is
+# defined as fib(n) = fib(n-1) + fib(n-2)
+def fibonacci(n):
+    pass
+
+# Finds the item i in the list l.... RECURSIVELY
+def find(l, i):
+    return None
+
+# Determines if a string is a palindrome
+def palindrome(some_string):
+    return False
+
+# Given the width and height of a sheet of paper, and the number of times to fold it, return the final dimensions of the sheet as a tuple. Assume that you always fold in half along the longest edge of the sheet.
+def fold_paper(width, height, folds):
+    return (0, 0)
+
+# Count up
+# Print all the numbers from 0 to target
+def count_up(target, n):
+    return
